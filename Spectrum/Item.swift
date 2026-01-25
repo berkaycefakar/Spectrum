657b57fefe4fe7918a628da1081905caa390239b
berkay cefakar <c.berkay@justsnap.de>
2026-01-25 22:25:10 +0300

    Initial Commit

---
+++ Spectrum/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Spectrum
+//
+//  Created by Berkay on 25.01.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ SpectrumTests/SpectrumTests.swift
@@ -0,0 +1,16 @@
+//
+//  SpectrumTests.swift
+//  SpectrumTests
+//
+//  Created by Berkay on 25.01.2026.
+//
+
+import Testing
+
+struct SpectrumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
